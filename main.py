c85b2dfb9da5718b4b0ce0ea27e35aa2fd82b720
Juan Gallostra <juangallostra@gmail.com>
2020-03-30 12:00:16 +0200

    Push points inside track limits

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ RED = [255, 0, 0]
 BLUE = [0, 0, 255]
 GRASS_GREEN = [58, 156, 53]
 
+MARGIN = 40
+
 ## logical functions
-def random_points(min=10, max=20, margin=30):
+def random_points(min=10, max=20, margin=MARGIN, min_distance=40):
     pointCount = rn.randrange(min, max+1, 1)
     points = []
     for i in range(pointCount):
         x = rn.randrange(margin, WIDTH - margin + 1, 1)
         y = rn.randrange(margin, HEIGHT -margin + 1, 1)
-        points.append((x, y))
+        distances = list(filter(lambda x: x < min_distance, [math.sqrt((p[0]-x)**2 + (p[1]-y)**2) for p in points]))
+        if len(distances) == 0:
+            points.append((x, y))
     return np.array(points)
 
 def get_track_points(hull, points):
@@ -34,7 +38,7 @@ def make_rand_vector(dims):
     mag = sum(x**2 for x in vec) ** .5
     return [x/mag for x in vec]
 
-def shape_track(track_points, difficulty=0.15, max_displacement=50):
+def shape_track(track_points, difficulty=0.15, max_displacement=50, margin=MARGIN):
     track_set = [[0,0] for i in range(len(track_points)*2)] 
     for i in range(len(track_points)):
         displacement = math.pow(rn.random(), difficulty) * max_displacement
@@ -43,8 +47,21 @@ def shape_track(track_points, difficulty=0.15, max_displacement=50):
         track_set[i*2 + 1][0] = int((track_points[i][0] + track_points[(i+1)%len(track_points)][0]) / 2 + disp[0])
         track_set[i*2 + 1][1] = int((track_points[i][1] + track_points[(i+1)%len(track_points)][1]) / 2 + disp[1])
     for i in range(3):
+        track_set = fix_angles(track_set)
         track_set = push_points_apart(track_set)
-    return track_set
+    # push any point outside limits back again
+    final_set = []
+    for point in track_set:
+        if point[0] < margin:
+            point[0] = margin
+        elif point[0] > (WIDTH - margin):
+            point[0] = WIDTH - margin
+        if point[1] < margin:
+            point[1] = margin
+        elif point[1] > HEIGHT - margin:
+            point[1] = HEIGHT - margin
+        final_set.append(point)
+    return final_set
 
 def push_points_apart(points, distance=30):
     # distance might need some tweaking
@@ -65,6 +82,38 @@ def push_points_apart(points, distance=30):
                 points[j][1] = int(points[j][1] + dy);  
                 points[i][0] = int(points[i][0] - dx);  
                 points[i][1] = int(points[i][1] - dy);  
+    return points
+
+def fix_angles(points, max_angle=100):
+    for i in range(len(points)):
+        if i > 0:
+            prev_point = i - 1
+        else:
+            prev_point = len(points)-1
+        next_point = (i+1) % len(points)
+        px = points[i][0] - points[prev_point][0]
+        py = points[i][1] - points[prev_point][1]
+        pl = math.sqrt(px*px + py*py)
+        px /= pl
+        py /= pl
+        nx = -(points[i][0] - points[next_point][0])
+        ny = -(points[i][1] - points[next_point][1])
+        nl = math.sqrt(nx*nx + ny*ny)
+        nx /= nl
+        ny /= nl  
+        a = math.atan2(px * ny - py * nx, px * nx + py * ny)
+        if (abs(math.degrees(a)) <= max_angle):
+            continue
+        nA = math.radians(max_angle * math.copysign(1,a))
+        diff = nA -a
+        c = math.cos(diff)
+        s = math.sin(diff)
+        new_x = nx * c - ny * s
+        new_y = nx * s + ny * c
+        new_x *= nl  
+        new_y *= nl
+        points[next_point][0] = int(points[i][0] + new_x)
+        points[next_point][1] = int(points[i][1] + new_y)
     return points
 
 ## drawing functions
